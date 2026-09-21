39f62303b5f95e98f67bc7b8b189a43abb68dea9
Aditi Khare <toaditikhare@gmail.com>
2026-09-21 22:23:44 +0000

    remove hardcoding

--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
-const API_URL = 'http://accdda5a326a24493a81f1394950bf43-604742334.us-east-1.elb.amazonaws.com';
+
+const API_URL = process.env.REACT_APP_MOVIE_API_URL;
 
 function MovieList({ onMovieClick }) {
   const [movies, setMovies] = useState([]);
