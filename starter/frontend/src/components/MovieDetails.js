c31259a615cd6ca4e3cf94f8d82591e514e5411b
Aditi Khare <toaditikhare@gmail.com>
2026-09-21 21:47:32 +0000

    fix ci

--- starter/frontend/src/components/MovieDetails.js
+++ starter/frontend/src/components/MovieDetails.js
@@ -2,12 +2,15 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
+const API_URL =
+  'http://accdda5a326a24493a81f1394950bf43-604742334.us-east-1.elb.amazonaws.com';
+
 function MovieDetails({ movie }) {
   const [details, setDetails] = useState(null);
 
   useEffect(() => {
     if (!movie) return;
-    axios.get(`http://accdda5a326a24493a81f1394950bf43-604742334.us-east-1.elb.amazonaws.com/movies/${movie.id}`).then((response) => {
+    axios.get(`${API_URL}/movies/${movie.id}`).then((response) => {
       setDetails(response.data);
     });
   }, [movie]);
--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
+const API_URL =
+  'http://accdda5a326a24493a81f1394950bf43-604742334.us-east-1.elb.amazonaws.com';
+
 function MovieList({ onMovieClick }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    axios.get('http://accdda5a326a24493a81f1394950bf43-604742334.us-east-1.elb.amazonaws.com/movies').then((response) => {
+    axios.get(`${API_URL}/movies`).then((response) => {
       setMovies(response.data.movies);
     });
   }, []);
@@ -14,7 +17,11 @@ function MovieList({ onMovieClick }) {
   return (
     <ul>
       {movies.map((movie) => (
-        <li className="movieItem" key={movie.id} onClick={() => onMovieClick(movie)}>
+        <li
+          className="movieItem"
+          key={movie.id}
+          onClick={() => onMovieClick(movie)}
+        >
           {movie.title}
         </li>
       ))}
