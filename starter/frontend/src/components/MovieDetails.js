21603b45f696f3bf76f05d4b2e5995416e629ada
Aditi Khare <toaditikhare@gmail.com>
2026-09-21 19:17:25 +0000

    file fix

--- starter/frontend/src/components/MovieDetails.js
+++ starter/frontend/src/components/MovieDetails.js
@@ -1,20 +1,27 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import axios from 'axios';
 
 function MovieDetail({ movie }) {
   const [details, setDetails] = useState(null);
+
   useEffect(() => {
-    axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies/${movie.id}`).then((response) => {
+    if (!movie) return;
+    axios.get(`/movies/${movie.id}`).then((response) => {
       setDetails(response.data);
     });
   }, [movie]);
 
   return (
     <div>
-      <h2>{details?.movie.title}</h2>
-      <p>{details?.movie.description}</p>
+      <h2>{details?.movie?.title}</h2>
+      <p>{details?.movie?.description}</p>
     </div>
   );
 }
+
+MovieDetail.propTypes = {
+  movie: PropTypes.object,
+};
 
 export default MovieDetail;
